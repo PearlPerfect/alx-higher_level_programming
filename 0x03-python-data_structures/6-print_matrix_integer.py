6166531306820c62670ca9d4a05d258737feef0c
PearlPerfect <PearlPerfect>
2023-03-13 04:17:14 -0700

    project 3

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    x = len(matrix)
+    if not matrix[0]:
+        print()
+    for j in range(x):
+        y = len(matrix[j])
+        for i in range(y):
+            if i == y - 1:
+                print("{:d}".format(matrix[j][i]))
+            else:
+                print("{:d} ".format(matrix[j][i]), end="")
