5636dad25f31861a98ca897731ff72ce64cc739e
PearlPerfect <PearlPerfect>
2023-04-11 07:50:42 -0700

    corrections

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -5,12 +5,13 @@
 class Rectangle:
     '''
     The Rectangle class represents a rectangle object.
+    
     Attributes:
         width (int): The width of the rectangle.
         height (int): The height of the rectangle.
+    
     Methods:
         area(): Returns the area of the rectangle.
         perimeter(): Returns the perimeter of the rectangle.
     '''
     pass
-
