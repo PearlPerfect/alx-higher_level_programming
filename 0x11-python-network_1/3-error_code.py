0758d534b8edcd2e0b304e0ea70b02b71f3dd027
PearlPerfect <zenithyonex4life@gmail.com>
2024-01-26 11:08:50 +0100

    new

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a script that takes in a URL, sends a request to same and displays
+the body of the response (decoded in utf-8).
+"""
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    requests = urllib.request.Request(url=url)
+
+    try:
+        with urllib.request.urlopen(requests) as response:
+            response_body = response.read()
+            print(response_body.decode("utf-8"))
+    except urllib.error.HTTPError as error:
+        print(f"Error code: {error.code}")
---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a script that takes in a URL, sends a request to same
+and displays the body of the response."""
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    error_code = req.status_code
+
+    if error_code >= 400:
+        print(f'Error code: {error_code}')
+    else:
+        print(req.text)
